c7a4c412b413ddb097a0461e1e31fdfdd17f6e34
hannahhughes <hannahhughes@seattleacademy.org>
2016-02-12 11:04:23 -0800

    add all things

--- 03_hannah.js
+++ 03_hannah.js
@@ -1,13 +1,11 @@
+port = process.argv[2];
+var port = (process.argv[2] ? Number(process.argv[2]) : 2002);
 var net = require('net');
-//console.log('net',net);
 var server = net.createServer(respond);
 
-function respond(socket){
-	d = new Date();
-	socket.end(d.toString());
+function respond(socket) {
+    d = new Date();
+    socket.end(d.toString());
 }
-
-port = process.argv[2];
-//TODO check for undefined or bad port
-console.log("listening of port", port);
+console.log("Listening on port ", port);
 server.listen(port);
--- 03_template.js
+++ 03_template.js
@@ -1,5 +1,5 @@
 port = process.argv[2];
-var port = (process.argv[2] ? Number(process.argv[2]) : 3000);
+var port = (process.argv[2] ? Number(process.argv[2]) : 2002);
 var net = require('net');
 var server = net.createServer(respond);
 
